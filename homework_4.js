cb9da26f1ebd8c8ca3ab31e5e8fa6438f2ea1a25
Ksenia Luzanova <68735083+Luzanova-Ksenia@users.noreply.github.com>
2020-09-09 21:55:59 +0300

    Add files via upload

---
+++ homework_4.js
@@ -0,0 +1,183 @@
+/**
+ * fruits arr is a store of fruits, fill as you wish
+ * Example
+ * ['banana', 'orange', 'orange']
+ */
+const fruits = ['peach', 'grapes', 'banana', 'orange', 'banana', 'peach', 'grapes'];
+/**
+ * fruitsBought arr is a store of bought fruits
+ * push fruit here after buying
+ */
+
+const fruitsBought = [];
+/**
+ * logs array is used to store logs on the page.
+ * Can be filled with executing log function
+ * 
+ * Example:
+ * log(customerName, fruitName, success)
+ * where
+ * - customerName is name of customer
+ * - fruitName is name of fruit
+ * - success is state if customer bought a fruit
+ */
+const logs = []; 
+
+/**
+ * Example of customer
+ */
+const customers = [
+    {
+    name: 'Ivan',
+    balance: 46,
+    fruitsToBuy: [{
+        name: 'banana',
+        count: 4,
+	}]},
+	{
+        name: 'Vlad',
+    balance: 73,
+    fruitsToBuy: [{
+        name: 'orange',
+        count: 2,
+	}]},
+	{
+        name: 'Senia',
+    balance: 84,
+    fruitsToBuy: [{
+        name: 'grapes',
+        count: 1,
+    }]}
+];
+
+/**
+ * Function buyFruits is used to iterate over array passes as a param
+ * if customer wants to buy 4 banana try to remove banana from fruits array (find it above)
+ * and push it to fruitsBought array (find it above)
+ * 
+ * Push execution result in log
+ * function log is written below, you'll find execution example on line 18 of this file
+ * 
+ * Example:
+ * buyFruits(customers)
+ */
+ 
+function buyFruits(customersArr) {
+    // write code here
+	//console.log(Object.keys(customersArr).length)
+	//console.log(customersArr[0].fruitsToBuy[0].name)
+    for (let i = 0; i < Object.keys(customersArr).length; i++) {
+		//console.log(customersArr[i])
+		const fru = String(customersArr[i].fruitsToBuy[0].name)
+		if (fruits.includes(fru)){
+			fruitsBought.push(fru)
+			const In = (fruits.indexOf(fru))
+			fruits.splice(In,1)
+		}
+		console.log(fruitsBought)
+		console.log(fruits)
+	}
+	return fruitsBought
+	return fruits
+};
+
+//buyFruits(customers)
+/**
+ * Function getFruitsMap returns map of fruits
+ * Example:
+ * 
+ * const fr = ['banana', 'orange', 'orange']
+ * getFruitsMap(fr); -> { banana: 1, orange: 2 }
+ * 
+*/ 
+
+ function getFruitsMap(fruitsArr) {
+    // write code here
+	//console.log(fruitsArr)
+	let count = {}
+	for (let elem of fruitsArr) {
+		if (count[elem] === undefined) {
+			count[elem] = 1;
+		} else {
+			count[elem]++;
+		}
+	}
+	console.log(count)
+	return count;
+	//const fruCount = fruitsArr.reduce(function (FruMap, fra) {
+	//	if (fra[] in FruMap === false) {
+	//		FruMap[fra] = 0
+	//	}
+	//	
+	//	FruMap[fra] += 1
+	//	return FruMap
+	//	
+	//}, {});	
+	//console.log(FruMap)
+ }
+
+//getFruitsMap(fruitsBought)
+
+// DONT'T EDIT FOLLOWING CODE
+buyFruits(customers);
+const fruitsLeftMap = getFruitsMap(fruits);
+const fruitsBoughtMap = getFruitsMap(fruitsBought);
+
+
+function log (customerName, fruitName, success) {
+    const action = success ? 'bought' : 'faliled to buy';
+    const className = success ? 'green' : 'red'
+    logs.push(`${customerName} <span class=${className}>${action}</span> ${fruitName}`);
+}
+
+function render () {
+    renderLog();
+    renderFruits('fruits-left', fruitsLeftMap);
+    renderFruits('fruits-bought', fruitsBoughtMap);
+
+    function renderLog() {
+        let existingLogsContainer = document.getElementById('messages');
+        let mainLogsContainer =  document.getElementById('log');
+        
+        if (existingLogsContainer) {
+            mainLogsContainer.removeChild(existingLogsContainer);
+        }
+        
+        const logsContainer = document.createElement('ul');
+        logsContainer.id = 'messages';
+        
+        logs.forEach(log => {
+            const logEl = document.createElement('li');
+            logEl.innerHTML = log;
+            logsContainer.appendChild(logEl);
+        });
+        
+        mainLogsContainer.appendChild(logsContainer);    
+    }
+    
+    function renderFruits (id, mappedData) {
+        const mainFruitsContainer = document.getElementsByClassName(id)[0];
+        const existingFruitsBoughtContainer = (mainFruitsContainer.getElementsByClassName('fruits-list') || [])[0];
+        
+        if (existingFruitsBoughtContainer) {
+            mainFruitsContainer.removeChild(existingFruitsBoughtContainer);
+        }
+        
+        const fruitsContainer = document.createElement('ul');
+        fruitsContainer.classList.add('fruits-list');
+        
+        for (let key in mappedData) {
+            const fruitEl = document.createElement('li');
+            const nameEl = document.createElement('span');
+            const countEl = document.createElement('span');
+            nameEl.innerText = `${key}:`;
+            countEl.innerText = mappedData[key];
+        
+            fruitEl.appendChild(nameEl);
+            fruitEl.appendChild(countEl);
+            fruitsContainer.appendChild(fruitEl);
+        }
+        
+        mainFruitsContainer.appendChild(fruitsContainer);
+    }
+}
